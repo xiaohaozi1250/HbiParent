45c1a1f56ec2963a17f5fcc808fc9e69da891d3c
ValZhang <wei.zhang12@hand-china.com>
2018-12-04 18:19:22 +0800

    new

--- core/src/main/java/com/hand/hap/demo/controllers/OmHeadersController.java
+++ core/src/main/java/com/hand/hap/demo/controllers/OmHeadersController.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.validation.BindingResult;
+
 import java.util.List;
 
 @Controller
